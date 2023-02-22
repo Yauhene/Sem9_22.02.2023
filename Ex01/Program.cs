6d8d146f2b57b86e6deb1a3a387ba72fea652466
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-22 21:53:26 +0300

    commit #1

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,4 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿// // See https://aka.ms/new-console-template for more information
+// //Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.// Собрать строку с числами от a до b, a <= b
+
+// string NumbersFor(int a, int b) // сам метод
+// {
+// string result = String.Empty;
+// for (int i = a; i <= b; i++)
+// {
+// result += $"{i} ";
+// }
+// return result;
+// }
+
+// string NumbersRec(int a, int b) // тот же метод, но рекурсивный
+// {
+// if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+// else return String.Empty;
+// }
+
+// Console.Clear();
+// Console.WriteLine(NumbersFor(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+// Console.WriteLine(NumbersRec(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+
+// Console.WriteLine();
+// // 
+
+// string NumbersRec(int a, int b) // тот же метод, но рекурсивный
+// {
+// if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+// else return String.Empty;
+// }
+
+// Console.Clear();
+
+// Console.WriteLine(NumbersRec(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+
+// Console.WriteLine();
 
 
